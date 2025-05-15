01fea2c7124de6af595e6f170e69c998d479d7b9
Group5_II-A_NETAD_DEQUIROZ_CARPIO_TOLENTINO_CORDERO <dequirozjanpaul123@gmail.com>
2025-05-15 15:49:23 +0800

    Fourth Commit/Carpio,DeQuiroz,Cordero and Tolentino

--- app/src/main/java/com/example/polypath/koreanlanguage.java
+++ app/src/main/java/com/example/polypath/koreanlanguage.java
@@ -1,15 +1,21 @@
 package com.example.polypath;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class koreanlanguage extends AppCompatActivity {
+
+    MediaPlayer mysound;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_koreanlanguage);
+        mysound = MediaPlayer.create(koreanlanguage.this, R.raw.annyeonghaseyo);
 
         Button btnBack = findViewById(R.id.btnBack);
         btnBack.setOnClickListener(v -> {
@@ -17,5 +23,9 @@ public class koreanlanguage extends AppCompatActivity {
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
             startActivity(intent);
         });
+    }
+
+    public void korean(View v) {
+        mysound.start();
     }
 }
